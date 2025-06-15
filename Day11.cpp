7fb64e9e959f81a1accbf42c8d77c8b2233af62f
JayaTripathi027 <147529770+JayaTripathi027@users.noreply.github.com>
2025-06-15 15:38:39 +0530

    Add files via upload

---
+++ Day11.cpp
@@ -0,0 +1,21 @@
+//PRINTING FACTORS OF A NUMBER
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter a number" << endl;
+    cin >> num;
+
+    for (int i = 1; i <= num; i++)
+    {
+        if (num % i == 0)
+            cout << i << " ";
+    }
+
+    return 0;
+}
+//TIME COMPLEXITY is O(n)
+
+
